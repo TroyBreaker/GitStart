80ee00d26fbd03dcaa444b4188d5e42acd4f8832
Oleg <khudoleev.o@gmail.com>
2022-10-25 09:22:34 +0300

    del readme

--- script.cpp
+++ script.cpp
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 // Help me and us
 =======
 // Help me and us lol
 >>>>>>> 235a81fd07270077e29cc47c87d8d82369b5d7eb
+=======
+// Help me and us
+>>>>>>> a158b17 (change cpp in readme)
